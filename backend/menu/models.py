f6ff2789396ebaf93bcb0de3268f1b884849b573
root <root@ip-172-31-3-161.ap-south-1.compute.internal>
2024-11-23 17:29:44 +0000

    first commit,backbone setup

---
+++ backend/menu/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
