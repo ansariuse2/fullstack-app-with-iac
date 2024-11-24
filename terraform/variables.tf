625d59c2b155fed80402d24d875d5d59cd4c77c0
Musthak <musthak@aventusinformatics.com>
2024-11-24 20:25:08 +0530

    variable name changed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,10 @@
 variable "valoriz_subnet_cidr" {
     type = string
-    default = "192.168.0.0/24"
+    default = "192.168.1.0/25"
 }
 variable "valoriz_vpc_cidr" {
     type = string
-    default = "192.168.0.0/16"
+    default = "192.168.0.0/24"
 }
 
 variable "node_role_arn" {
