07becdec6514ec507f88aa4c4dcb04f0dac30d13
Musthak <musthak@aventusinformatics.com>
2024-11-24 20:04:14 +0530

    cidr block cariable name bug fixed

--- terraform/module/network/vpc/vpc.tf
+++ terraform/module/network/vpc/vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "valoriz_vpc" {
     tags = {
       Name:"VPC_FOR_VALORIZ"
     }
-    cidr_block = "var.valoriz_vpc_cidr_block"
+    cidr_block = "var.valoriz_vpc_cidr"
     enable_dns_hostnames = true
     enable_dns_support = true
 }
